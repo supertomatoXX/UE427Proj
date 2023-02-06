d0b6db323b4f7b2426ce8ff28b859a7b96470e15
supertomato <59599339@qq.com>
2023-02-06 17:02:59 +0800

    new Proj

---
+++ Proj/Source/Proj/ProjGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_API AProjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
